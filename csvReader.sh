d8e3e934689cfa0f541f6b79387542b52525083a
Chameera <chameera9019@gmail.com>
2018-06-07 12:54:37 +0700

    csvReader.sh updated

--- csvReader.sh
+++ csvReader.sh
@@ -9,4 +9,8 @@ if [ -f $output ]; then
 	rm -f $output
 fi
 
+sed -n -e 's/, [^ ]*/;/w  result.txt' input.csv
+sed -i '' '$ s/.$//' result.txt
+tr -d '\n' < result.txt >> $output
 
+rm -f result.txt
